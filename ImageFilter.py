dc27567181dc8076d86ab6d72436887bbdf1a452
Toygoon <toygoon@gmail.com>
2022-11-06 01:21:12 +0900

    이미지 필터 모듈 생성

---
+++ ImageFilter.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+from skimage.exposure import rescale_intensity
+
+
+class ImageFilter:
+    def __init__(self, img: str):
+        self.img = img
+        self.mask = None
+
+    def mean(self, size: int):
+        self.mask = np.full((size, size), 1 / (size * size))
+        return self.convolution()
+
+    def convolution(self):
+        # H : Height, W : Width
+        # 원본 이미지의 크기를 나타냄
+        (H, W) = self.img.shape[:2]
+
+        # MH : Mask Height, MW : Mask Width
+        # Mask의 크기를 나타냄
+        (MH, MW) = self.mask.shape[:2]
+
+        # P : Padding
+        # 테두리 부분은 이미지를 그대로 Convolution 할 수 없으므로,
+        # 크기를 늘려야 하며, 이미지의 추가 공간을 위한 크기
+        P = (MW - 1) // 2
+
+        # 추가 공간을 만듬
+        img = cv2.copyMakeBorder(self.img, P, P, P, P, cv2.BORDER_REPLICATE)
+        # 결과를 저장할 공간을 만들어 둠
+        out = np.zeros((H, W), dtype="float32")
+
+        for y in np.arange(P, H + P):
+            for x in np.arange(P, W + P):
+                # 픽셀 별로 저장하기 위해, 현재 연산 중인 부분을
+                # Region of Interest로 뽑음
+                roi = img[y - P:y + P + 1, x - P:x + P + 1]
+
+                # 계산된 값을 출력 이미지에 저장함
+                out[y - P, x - P] = (roi * self.mask).sum()
+
+        # rescale_intensity를 이용해 [0, 255] 범위를 벗어나는 부분을 보정
+        out = rescale_intensity(out, in_range=(0, 255))
+        # 이미지 형식인 uint8로 변경
+        out = (out * 255).astype("uint8")
+
+        # 이미지 출력
+        return out
+
+
+if __name__ == "__main__":
+    filter = ImageFilter(cv2.imread('./imgs/noisyimg.png'))
+
+    cv2.imshow('img', filter.mean(3))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+    # img = cv2.imread('./imgs/noisyimg.png')
+    # mask = np.full((3, 3), 1 / 9)
+    # filter = ImageFilter(img, mask)
+    # cv2.imshow('img', filter.convolution())
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    # filter = ImageFilter()
