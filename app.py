ff96dec8246f423191e14d01d6084c25df0a1557
Toygoon <toygoon@gmail.com>
2022-11-05 09:26:50 +0900

    MainWindow 클래스 추가

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from PyQt6.QtWidgets import QApplication, QWidget
+from PyQt6.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
 
 import sys
 
-app = QApplication(sys.argv)
 
-window = QWidget()
-window.show()
+class MainWindow(QMainWindow):
+    def __init__(self):
+        # QMainWindow 호출
+        super().__init__()
 
-app.exec()
+        # Window 이름 설정
+        self.setWindowTitle('Filtering')
+
+        # 버튼 생성
+        button = QPushButton('Button')
+        # View에 버튼 추가
+        self.setCentralWidget(button)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec()
